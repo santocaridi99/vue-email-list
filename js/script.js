3185875b7e72bf3642b6181385c54e999534f294
santocaridi99 <santocaridi99@gmail.com>
2022-01-24 15:38:09 +0100

    commentato il mounted (riga 9 js)

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ new Vue({
         // creo un array che chiamo mail che conterrà le 10 mail che dovrò inserire tramite API
         mails:[]
     },
+    // quando html sarà "montato" nel browser fai chiamata api
     mounted(){
+        // da 0 a 10 prendi mail dall'api tramite axios e pusha il suo data.response dentro l'array dichiarato sopra
         for(let i=0;i<10;i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
                 this.mails.push(result.data.response)
